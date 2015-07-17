1fc2fb1a7bbe4b819bfce3601541653faf91bdeb
Shawn <shawn.bulger@spaceconcordia.ca>
2015-07-17 18:21:47 -0400

    I created some directories for file organization, started expanding the README.md, created a stub for unit testing

---
+++ functions/ISA_model_test.m
@@ -0,0 +1,16 @@
+% External resources: http://www.mathworks.com/matlabcentral/fileexchange/39325-density-altitude-calculator 
+
+% This test is for validation of the ISA_model output
+% The ISA_model.m takes in ground temperature, ground pressure, and actual altitude, and outputs the density of air (rho)
+
+% The test will ensure the correct determination of density by comparing by tabulated values
+
+% import tabulated pressure, temperature, alitude, density values
+% TODO
+
+% calculate density using ISA_model function
+% TODO
+
+% assert calculated values of density versus tabulated values of density
+% TODO
+
---
+++ functions/basic_drag.m
@@ -0,0 +1,26 @@
+function [ C ] = basic_drag( input_args )
+% The following calculates the basic drag coefficient based on C.P. Hoult's
+% "Drag Coefficient (rev 3.2)". The drag is divided into skin friction
+% drag, pressure drag, wave drag and parasitic drag. Note that this
+% calculation assumes a nose with a Karman profile is being used
+
+% BASIC CONSTANTS DEFINITION
+%   gamma_air   = the ratio of specific heats for air (ul)
+%   R           = gas constant for air
+
+% SKIN FRICTION
+% Skin friction drag is caused by the shear of the air near the surface due
+% to the roughness of the surface it is attempting to travel against.
+% The following treatment of skin friction drag is broken into a
+% calculation of the skin friction drag on the body and on the fins.
+
+% Body Skin Friction
+
+% The following uses the equation if total wetted area vs. body station to
+% obtain the integral of the previously stated equation.
+% The equation of the nose profile is known to be:
+%
+% 
+
+
+end
