9fb6d95cedad5d29b2cac581475d5b63c0aeeecb
Shawn <shawn.bulger@spaceconcordia.ca>
2015-07-28 21:29:05 -0400

    Creating directory structure, adding stubs for drag models and their unit tests

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rocketry-performance
+
+This repo tracks the Matlab performance model for the rocketry team.
+
+Included are the following:
+- models: Simulink Models
+- functions: Matlab Functions, including tests
+
---
+++ functions/coef_drag_skin_friction.m
@@ -0,0 +1,5 @@
+function COEF_drag_skin_friction(A_wet_fins, A_ref, C_f_c, f_b, t, c)
+
+% Skin Friction Drag is a component of parasitic drag
+% The skin friction drag coefficient is a function of the wetted area, the reference area, the boyant force
+
