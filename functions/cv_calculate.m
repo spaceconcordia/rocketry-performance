95b1666e39e532a1a151a91028d0b01d3cb73885
Shawn <shawn.bulger@spaceconcordia.ca>
2015-09-07 19:56:57 -0400

    Added cg_calculation stub

---
+++ functions/cv_calculate.m
@@ -0,0 +1,4 @@
+function cg = cg_calculate(rocket_mass, motor_mass_dry, motor_mass_wet, motor_mass_delta)
+
+% As fuel is exausted, the center of gravity shifts
+% This model computes the center of gravity of the rocket during flight
