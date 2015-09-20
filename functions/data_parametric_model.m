5e6929209e24147a2d9d7a90544e860c877c1ace
Shawn <shawn.bulger@spaceconcordia.ca>
2015-09-20 16:08:21 -0400

    Reorganizing files, adding documentation and data directories

--- functions/data_parametric_model.m
+++ functions/data_parametric_model.m
@@ -4,9 +4,14 @@
 %
 % Data Access of Parametric Model
 %
-% Design Parameters for the Rocket are stored in a spreadsheet. These parameters
-% will be accessed and passed along into the simulation to determine other 
-% parameters and ultimately to evaluate the performance of those design values
+% Thrust curves are provided in tabulated and time-based form. The values for 
+% thrust need to be processed as input to the model in a synchronized fashion
+% with the simulation clock
+%
+% Additionally, Design Parameters for the Rocket are stored in a spreadsheet. 
+% These parameters will be accessed and passed along into the simulation to 
+% determine other parameters and ultimately to evaluate the performance of those 
+% design values
 %
 %--------------------------------------------------------------------------------
 % References
@@ -16,9 +21,12 @@
 
 % Import the data from Excel for a lookup table
 data = xlsread('Parametric_Model','Sheet1');
+
 % Row indices for lookup table
 breakpoints1 = data(2:end,1)';
+
 % Column indices for lookup table
 breakpoints2 = data(1,2:end);
+
 % Output values for lookup table
 table_data = data(2:end,2:end);
