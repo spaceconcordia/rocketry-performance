bbc2674089c0bcbac45bf03c0284b4c3bcaf49a6
Shawn <shawn.bulger@spaceconcordia.ca>
2015-11-08 02:30:39 -0500

    Parameterized the drag model. Now achieving more sensible output, however a singularity still forms near the end of the simulation, as the rocket hits the ground.

--- functions/area/AreaReferenceFrontal.m
+++ functions/area/AreaReferenceFrontal.m
@@ -1,10 +1,14 @@
 function area_reference_frontal = AreaReferenceFrontal(inputs)
 
+%-------------------------------------------------------------------------------
+% This function calculates the area of the rocket from the frontal reference
+% at an angle attack of zero
+%-------------------------------------------------------------------------------
 outer_diameter = inputs(1);
 number_of_fins = inputs(2);
 fin_thickness  = inputs(3);
 height_of_fins = inputs(4);
 
-area_reference_frontal=3.14159*outer_diameter^2/4+fin_thickness*height_of_fins*number_of_fins;
+area_reference_frontal = (pi/4 * outer_diameter^2) + (fin_thickness * height_of_fins * number_of_fins);
 
 end
