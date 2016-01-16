802635b112df6432855330cd718648f80353d897
Shawn <shawn.bulger@spaceconcordia.ca>
2016-01-15 22:15:13 -0500

    Angular model is producing coherent output. I had to hard-code the center of pressure, as well as reverse the sign of the normal force in the simplified model, in order for the restoring moment to counteract the angle of attack. The rocket is currently unstable - we are missing the damping moment, and there appears to be some issues with the center of gravity calculation (it is constant while fuel is expended, however the model reference test previously ruled out this issue)

--- functions/dynamics/COPRocket.m
+++ functions/dynamics/COPRocket.m
@@ -1,13 +1,15 @@
 function cop_rocket = COPRocket(inputs)
 
-aoa               = inputs(1);
-sd_nosecone       = inputs(2);
-sd_bodytube       = inputs(3);
-sd_finset         = inputs(4);
-cop_nose_cone     = inputs(5);
-cop_body_tube     = inputs(6);
-cop_fin_set       = inputs(7);
-coef_normal_force = inputs(8);
+aoa                 = inputs(1);
+sd_nosecone         = inputs(2);
+sd_bodytube         = inputs(3);
+sd_finset           = inputs(4);
+sd_rocket_planform  = inputs(5);
+cop_nose_cone       = inputs(5);
+cop_body_tube       = inputs(6);
+cop_fin_set         = inputs(8);
+cop_rocket_planform = inputs(9);
+coef_normal_force   = inputs(10);
 
 % prevent division by zero
 if coef_normal_force == 0
@@ -20,6 +22,8 @@ numerator = ...
     ( cos(aoa) * sd_bodytube * cop_body_tube ) ...
     + ...
     ( cos(aoa) * sd_finset * cop_fin_set ) ...
+    + ...
+    ( cos(aoa) * sd_rocket_planform * cop_rocket_planform ) ...
     ;
 
-cop_rocket = numerator / coef_normal_force(1);
+cop_rocket = numerator / coef_normal_force;
