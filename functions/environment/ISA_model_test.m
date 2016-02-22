d61b7e816887aaeeb0fddbbb19e36ff3cb4d4c88
neil1993 <neil@spaceconcordia.ca>
2016-02-22 03:19:12 -0500

    Computation code finished
    The first version of the computation code is complete. Code for
    visualization is still missing. It will be implemented next so that data
    validation can continue

--- functions/environment/ISA_model_test.m
+++ functions/environment/ISA_model_test.m
@@ -58,7 +58,6 @@
 %                     atmospheric data
 %       DataPoints  = An array to hold the number of atmospheric data
 %                     points in each set to facilitate future calculations
-%
 %-------------------------------------------------------------------------%
 
 FileList = dir('*_array*.mat');
@@ -85,9 +84,78 @@ for n = 1:NumFiles
     
 end
 
+%-------------------------------------------------------------------------%
+% Results Array - Theoretical
+%   This section runs the ISAModel.m script and places the temperature and
+%   pressure at altitude in a 3D array. This computed array will only
+%   contain pressure and temperature and pressure data based on the initial
+%   conditions, just like the flight simulations shoul;d
+%
+%   Variables:
+%       TheoValues  = Array to hold all theoretically obtained values for
+%                     comparison against experimental data (columns:
+%                     pressure, altitude, temperature)
+%       inputs      = Array to hold input data for ISAModel.m
+%       outputs     = Array to hold output data from ISAModel.m
+%-------------------------------------------------------------------------%
 
-% calculate density using ISA_model function
-% TODO
+TheoValues = zeros(1,3,NumFiles); 
 
-% assert calculated values of density versus tabulated values of density
-% TODO
+for n = 1:NumFiles
+    
+    inputs(1) = AtmoData(1,3,n) + 273.15;  % T_in in Kelvin
+    inputs(2) = AtmoData(1,1,n) * 100;     % P_in in Pascals
+    inputs(3) = AtmoData(1,2,n);           % Alt_in in meters
+    
+    for j = 1:DataPoints(n)
+        
+        inputs(4) = AtmoData(j,2,n);
+        outputs = ISAModel(inputs);
+        
+        TheoValues(j,1,n) = outputs(2);
+        TheoValues(j,2,n) = inputs(4);
+        TheoValues(j,3,n) = outputs(1);
+        
+    end
+    
+end
+
+%-------------------------------------------------------------------------%
+% Error Calculation
+%   This section calculates the experimental error between the theoretical
+%   values and the experimental values. The values for error on each plot
+%   are stored in an array
+%
+%   Variables:
+%       AllError    = A 3D array which stores all the error values
+%                     (columns: Pressure error, Temperature error)    
+%       E           = Temporary variable to contain error values
+%-------------------------------------------------------------------------%
+
+AllError = zeros(1,2,NumFiles);
+
+for n = 1:NumFiles
+    
+    for j = 1:DataPoints(n)
+        
+        %Pressure error
+        E = abs((100*AtmoData(j,1,n)-TheoValues(j,1,n))/TheoValues(j,1,n));
+        AllError(j,1,n) = E*100;
+        
+        %Temperature error
+        E = (AtmoData(j,3,n)+273.15-TheoValues(j,3,n))/TheoValues(j,3,n);
+        AllError(j,2,n) = abs(E*100);
+        
+    end
+
+end
+
+%-------------------------------------------------------------------------%
+% Plot creation
+%   This section takes all the previous data and and creates several plots
+%   in a matlab figure. The plots are saved under the call sign with which
+%   the data was entered. As well, a summary figure is created to show the
+%   overall trend of the error
+%
+%-------------------------------------------------------------------------%
+
